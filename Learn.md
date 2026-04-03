6d13d263216298265a277ff660efcd0e64427520
Wilson <fkkr58@durham.ac.uk>
2026-04-03 22:20:19 +0100

    Updated lessons

--- Learn.md
+++ Learn.md
@@ -1,4 +1,6 @@
 # Haskell
+
+## Starting out
 - Haskell is a purely functional language
 - Haskell has type inference
 - Haskell is elegant and concise
@@ -8,10 +10,77 @@
 - !! is for 0 indexed index lookup '"hello"!! 0' -> h
 - Lists are compared in lexicographical order
 - For lists, head, tail, last, init
-- head, tail, last, init
-- length, null, reverse, (take, drop), maximum, minimum, sum, product, elem
+- head (first element), tail (remaining from head), last (last element), init (everything but last element)
+- length (of list), null (true if empty), reverse (list), (take, drop), maximum (of list), minimum (of list), sum (of list), product (of list), elem (is element of list)
 - Haskell is lazy 'take 24 [13,26..]' gives first 24 multiples of 24 instead of calculating infinite
 - Cycle, repeat are most used with take:
     - take 10 (repeat 5)
     - take 10 (cycle [1,2,3]) # This is for decronstructing list and getting the same patterns
+    - *replicate is easiset to use 'replicate 3 10'
 - Can use set ideas [x | x <- [50..100], mod x 7 == 3] for example
+- Whatever is before '|' will be the value of that element of x
+- For multiple lists you can have [x*y | x <- [2,3], y <- [3,4]]. This gives the combination of it
+- Tuples have fst and snd for getting 1st and 2nd things in paired tuples only
+- zip can combine pairs of lists 'zip [1..5] ['a'..'g']
+
+## Types
+- Give explicit type declaraton using :: ("has type of")
+- Integer is for big values, Int is more efficient for smaller numbers
+- Double has better precision than Float
+- Bool, Char, [Char] (also can use String)
+- Prefix functions are common, but you can change infix functions such as == by surrounding with brackets '(==) 5 5'
+- Typeclasses: Eq, Ord (type class that can be ordered with <, > etc), Show (ints to string for example), Read (string to int for example if in same context as other operations)
+- succ and pred can be used to find before and after 
+- minBound, maxBound can be used for int types usage: 'maxBound :: (Bool, Int, Char)'
+- Num (all numbers) covers both Integral (whole) and Floating (decimal)
+- fromIntegral converts numbers to more general (such as length [1..10])
+- This doesnt work for regular numbers such as '10 + 3.2' because 10 is treated as a polymorphic number literal not a fixed Int. Haskel can interpret by context
+
+Syntax
+- Alternative if stetement:
+    - lucky :: (Integral a) => a -> String
+    - lucky 7 = "LUCKY NUMBER SEVEN"
+    - lucky x = "Sorry your unlucky"
+- Can do error statements:
+    - head' :: [a] -> a
+    - head' [] = error "Can't call head on empty list"
+    - head' (x:_) = x
+- Parentheses are used to group elements of a list
+    - (x:y:_) for [x,y......]
+    - (x:y:[]) for [x,y]
+    - (x:[]) for [x]
+- Datatype classes allow for more generalisation
+    - Cannot do 'sum' :: [Num] -> Num'
+    - Have to separate like 'sum' :: Num a => [a] -> a'
+    - You cannot use types in the same way as typeclasses
+- Patterns:
+    - capital :: String -> String
+    - capital "" = "empty string"
+    - capital all@(x:xs) = "First letter of " ++ all ++ " is " ++ [x]
+- Guards:
+    - max' :: (Ord a) => a -> a -> a
+    - max' a b
+    -     | a < b = b
+    -     | otherwise = a
+- 'Ordering' data type - LT, EQ, GT
+    - myCompare :: (Ord a) => a -> a -> Ordering
+    - a `myCompare` b
+    -     | a > b     = GT
+    -     | a == b    = EQ
+    -     | otherwise = LT
+- Where statements can be used after guards to avoid recalculation
+    - densityTell :: (RealFloat a) => a -> a -> String  
+    - densityTell mass volume  
+    -     | density < air = "Wow! You're going for a ride in the sky!"  
+    -     | density <= water = "Have fun swimming, but watch out for sharks!"  
+    -     | otherwise   = "If it's sink or swim, you're going to sink."  
+    -     where 
+            density = mass / volume  
+    -       air = 1.2  
+    -       water = 1000.0
+- Case Expressions can be used like so where the case inbetween 'case [case] of '[pattern] -> etc'' is compared to the patterns:
+    - head' :: [a] -> a  
+    - head' xs = case xs of [] -> error "No head for empty lists!"  
+                        (x:_) -> x  
+- Since Int can be negative, you have to make sure you use guards instead of patterns
+- 'take 5 (repeat 3)' will give us a list of five 3’s
--- Lessons.hs
+++ Lessons.hs
@@ -1,5 +1,175 @@
 doubleMe x = x + x
 doubleUs x y = x*2 + y*2
 doubleSmallNumber x = (if x <= 100 then x*2 else x) + 1
+
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]  
 exclude x' = [x | x <-x', x/= 13, x/=15] 
+
+-- length' xs = sum [1| _ <- xs]
+removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z']]
+
+a = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
+removeOdd xs = [[x| x <- xd, even x]| xd <- xs]
+
+x' = [1..10]
+y = [(x, y, z) | x <- x', y <- x', z <- x', x + y + z == 24, x*x + y*y - z*z == 0, x < y]
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN"
+lucky x = "Sorry your unlucky"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- Pattern matching
+tuples = [(1,2), (3,4), (5,6)]
+pattern = [a + b | (a,b) <- tuples]
+
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' x = x !! 0  
+
+-- Length using recursion
+length' :: [a] -> Int
+length' [x] = 1
+length' (x:y) = 1 + length' y  
+
+-- Sum using recursion
+sum' :: (Integral a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+-- Patterns
+capital :: String -> String
+capital "" = "empty string"
+capital all@(x:xs) = "First letter of " ++ all ++ " is " ++ [x]
+
+-- Guards
+-- densityTell :: (RealFloat a) => a -> String
+-- densityTell density
+--     | density < 1.2 = "WOW FLYING"
+--     | density <= 1000.0 = "Swim"
+--     | otherwise = "You sinking"
+
+
+-- max' :: (Ord a) => a -> a -> a
+-- max' a b
+--     | a < b = b
+--     | otherwise = a
+
+-- Where statements
+densityTell :: (RealFloat a) => a -> a -> String  
+densityTell mass volume  
+    | density < air = "Wow! You're going for a ride in the sky!"  
+    | density <= water = "Have fun swimming, but watch out for sharks!"  
+    | otherwise   = "If it's sink or swim, you're going to sink."  
+    where 
+        density = mass / volume
+        air = 1.2  
+        water = 1000.0  
+
+-- Using Where statements in other contexts
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ [l]
+    where 
+        (f:_) = firstname
+        (l:_) = lastname
+
+calcDensities :: (RealFloat a) => [(a, a)] -> [a]  
+calcDensities xs = [density m v | (m, v) <- xs]  
+    where 
+        density mass volume = mass / volume  
+
+
+-- Max using recursion
+max' :: (Ord a) => a -> a -> a 
+max' a b = case a > b of 
+    True -> a
+    False -> b
+
+-- Maximum using recursion
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "No elements to look at"
+maximum' [a] = a 
+maximum' (a:xs) = max' a (maximum xs)
+
+-- Replicate using recursion (Use guards because Ints can be negative)
+replicate' :: Int -> b -> [b]
+replicate' a b
+    | a <= 0 = []
+    | otherwise = b : replicate (a - 1) b
+
+-- Take using recursion
+take' :: Int -> [b] -> [b]
+take' a _
+    | a <= 0 = []
+take' _ [] = []
+take' a (b:bs) = b : take' (a - 1) bs
+
+-- Reverse using recursion
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ (x :[])  
+
+-- Zip using recursion
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (a:as) (b:bs) = [(a, b)] ++ zip as bs 
+
+-- Elem using recursion
+elem' ::  (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (b:_)
+    | a == b = True
+elem' a (b:bs) = elem' a bs
+
+-- Quick sort using recursion
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) = smallest ++ [x] ++ biggest
+    where 
+        smallest = quicksort' [y | y <- xs, x > y]
+        biggest = quicksort' [y | y <- xs, y >= x]
+
+-- zipWidth using recursion
+zipWidth' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWidth' _ [] _ = []
+zipWidth' _ _ [] = []
+zipWidth' a (b:bs) (c:cs) = [a b c] ++ zipWidth' a bs cs
+
+-- flip using recursion
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' a b c = a c b
+
+-- map using recursion
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' a (b:bs) = [a b] ++ map' a bs  
+
+-- filter using recursion
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' a (b:bs)
+    | a b == True = b : filter' a bs
+    | otherwise = filter' a bs
+
+-- Example use case for filter:
+-- let notNull x = not (null x) in filter notNull [[1,2,3],[],[3,4,5],[2,2],[],[],[]]  
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain a
+    |  even a = let next = div a 2 in [a] ++ chain next
+    |  otherwise = let next = (a * 3) + 1 in [a] ++ chain next
+
+numLongChains :: Int
+numLongChains = sum [1 | x <- [1..100], length (chain x) > 15]
+
+numLongChains' :: Int  
+numLongChains' = length (filter isLong (map chain [1..100]))  
+    where isLong xs = length xs > 15  
